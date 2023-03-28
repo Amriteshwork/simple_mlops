1b3042b39902e2d98a20ef72c68ece52178eef71
Amritesh Nandan <amriteshwork@gmail.com>
2023-03-28 16:46:43 +0530

    github action workflow added

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,20 @@
+import pytest
+
+
+class NotInRange(Exception):
+    def __init__(self, message="value not in range") -> None:
+        self.message = message
+        super().__init__(self.message)
 
 
 def test_generic():
+    a = 5
+    with pytest.raises(NotInRange):
+        if a in range(3, 9):
+            raise NotInRange
+
+
+def test_somthing():
     a = 2
     b = 2
-    assert a == b
+    assert True
