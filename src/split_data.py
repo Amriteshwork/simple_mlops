428d460eb615efba09174e25353da51b48cc4c36
Amritesh Nandan <amriteshwork@gmail.com>
2023-03-28 12:53:15 +0530

    trackers added

--- src/split_data.py
+++ src/split_data.py
@@ -22,6 +22,8 @@ def split_and_save(config_path):
     train.to_csv(train_data_path, sep=",", index=False, encoding="utf-8")
     test.to_csv(test_data_path, sep=",", index=False, encoding="utf-8")
 
+
+
 if __name__ == "__main__":
     args =  argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
